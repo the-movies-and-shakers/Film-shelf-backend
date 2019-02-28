18850d041d08b30107fe29d1024bc2bb6a017f79
Cath W <catkenna@hotmail.com>
2019-02-28 18:00:17 +0000

    First GET function, not put into postman as no serverless link

---
+++ databaseservice.js
@@ -0,0 +1,32 @@
+const mysql =require ("mysql");
+
+function getDatabaseConnection() {
+    return mysql.createConnection({
+        host: process.env.RDS_HOST,
+        user: process.env.RDS_USER,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE
+    });
+}
+
+function getFilms() {
+    const connection = getDatabaseConnection();
+    return new Promise(function(resolve, reject) {
+        connection.query("SELECT * FROM filmshelf", function(error, results, fields) {
+            if (error) {
+                connection.destroy();
+                return reject(error);
+            } 
+            else {
+                connection.end(function() { 
+                return resolve(results);
+       
+                });
+            }
+        });
+    });
+}
+
+module exports = {
+    getFilms
+}
---
+++ films.js
@@ -0,0 +1,27 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+const databaseService = require('./databaseservice');
+
+
+
+
+app.get('/films', function (request, response) {
+
+  databaseService.getFilms()
+  .then(function(results){
+    response.json(results);
+
+})
+
+.catch (function(error){
+
+    response.status(500); 
+    response.json(error);
+});
+  
+ })
