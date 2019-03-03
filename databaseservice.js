6ec7f8e5502f0fc2ed6dd2f3697a23fa5dcda966
Kerry-Anne Kubisa <kerry-anne@kerry-annes-air.home>
2019-03-03 21:01:09 +0000

    KA POST & PUT

--- databaseservice.js
+++ databaseservice.js
@@ -45,8 +45,76 @@ function deleteFilm() {
     });
 }
 
+function saveFilm(filmTitle) {
+    const connection = getDatabaseConnection();
+
+    return new Promise(function(resolve, reject) {
+
+        const postData = {
+            Description: filmTitle,
+            userId: 1 //should this say filmId?
+        };
+        connection.query('INSERT INTO filmdata SET ?', postData, function (error, results, fields) {
+            if (error) {
+            connection.destroy();
+            return reject(error);
+        }
+            else {
+                connection.end(function() {
+                return resolve(results);
+
+                });
+            }
+        });
+    });
+}
+
+//I dont think we need this as we dont need to mark the film as being completed: -
+
+// function completeFilm(filmIdToComplete) {
+
+//     const connection = getDatabaseConnection();
+
+//     return new Promise(function(resolve, reject) {
+
+//         connection.query("UPDATE film SET Completed = 1 WHERE FilmID = ?", filmIdToComplete, function (error, results, fields) {
+//             if (error) {
+//                 connection.destroy();
+//                 return reject(error);
+//             }
+//             else {
+//                 connection.end(function() {
+//                     return resolve(results);
+//                 });
+//             }
+//         });
+//     });
+//  };
+
+function editFilm(editedFilmTitle, identifier) {
+
+    const connection = getDatabaseConnection();
+
+    return new Promise(function(resolve, reject) {
+
+        connection.query("UPDATE films SET filmTitle = ? WHERE filmId = ?", editedFilmTitle, identifier, function (error, results, fields) {
+            if (error) {
+                connection.destroy();
+                return reject(error);
+            }
+            else {
+                connection.end(function() {
+                    return resolve(results);
+                });
+            }
+        });
+    });
+ };
 
 module.exports = {
     getFilms,
-    deleteFilm
-}
+    deleteFilm,
+    saveFilm,
+    // completeFilm,
+    editFilm
+};
